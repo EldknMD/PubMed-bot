4f0f2d499ff3614174cc9b7755a3cf5b9cd68dea
EldknMD <pablodkn@gmail.com>
2019-08-19 17:10:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const Pubmed = require('node-ncbi');
+const ncbi = require('node-ncbi');
 const config = require('./config.json');
+const pubmed = ncbi.pubmed;
 
 console.log ("hello, world!")
 client.on("message", message => {
     if (message.content == "pub") {
-        message.channel.send("med")
+        message.channel.send("med")} {
 
-    }
+        
+    if (message.content == "diabetes") {
+pubmed.search('2 type diabetes').then((results) => {
+    var resultado = JSON.stringify(results)
+    message.channel.send(resultado)
+    console.log(results)
 })
+
+}}});
+
 client.login(config.token);
